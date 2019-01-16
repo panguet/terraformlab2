e5fb55270825bc0b207476cb3ee9e38b1b37c2c9
Anguet <patrick.anguet@me.com>
2019-01-16 18:05:35 +0100

    slice in specific ressource type files and create google server instance with debian-cloud/debian-9

--- subnetworks.tf
+++ subnetworks.tf
@@ -1,17 +1,4 @@
-###  this script will organize multicloud deployment with :
-### - AWS 
-### - Azure 
-### - google compute plateforme  (GCP)
-### version : 0.2
-
-
-# google Terraform deployment
-resource "google_compute_network" "our_development_network" {
-  name = "devnetwork"
-  project = "terraformlab2"
-  auto_create_subnetworks = false
-}
- 
+# Subnetworks part 
 resource "google_compute_subnetwork" "dev-subnet" {
   ip_cidr_range ="10.0.1.0/24"
   name = "devsubnet"
@@ -19,17 +6,6 @@ resource "google_compute_subnetwork" "dev-subnet" {
   region = "europe-west1"
  
 }
-
-# AWS Terraform deployment
-resource "aws_vpc" "enviromment-example-two" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  tags {
-      Name = "terraform-aws-vpc-example-two"
-  }
-}
-
 resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.enviromment-example-two.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.enviromment-example-two.id}"
@@ -41,6 +17,7 @@ resource "aws_subnet" "subnet2" {
   vpc_id = "${aws_vpc.enviromment-example-two.id}"
   availability_zone = "eu-west-1b"
 }
+
 resource "aws_security_group" "subnetsecurity" {
   vpc_id ="${aws_vpc.enviromment-example-two.id}"
 
@@ -57,14 +34,6 @@ resource "aws_security_group" "subnetsecurity" {
   }
 }
 
-
-
-# Azure Terraform deployment
-resource "azurerm_resource_group" "azy_network" {
-  location = "France Central"
-  name = "devresgrp"
-  
-}
 
 resource "azurerm_virtual_network" "blue_virtual_network" {
   address_space = ["10.0.0.0/16"]
