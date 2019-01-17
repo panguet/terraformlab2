aabe0805be601fc9d961657f68b0c008b2968879
Anguet <patrick.anguet@me.com>
2019-01-17 17:13:29 +0100

    aws ubuntu second server

--- server.tf
+++ server.tf
@@ -1,3 +1,4 @@
+# google Compute instance 
 resource "google_compute_instance" "firstserver" {
     name = "thefirstserver"
     machine_type = "n1-standard-1"
@@ -24,4 +25,35 @@ resource "google_compute_instance" "firstserver" {
     service_account {
         scopes = ["userinfo-email" , "compute-ro" , "storage-ro"]
     }
+}
+
+# AWS EC2 instance 
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners = ["099720109477"]
+
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+
+    }
+    filter {
+        name = "image-id"
+        values = ["ami-844e0bf7"]
+    }
+}
+
+resource "aws_instance" "seconserver" {
+    ami = "${data.aws_ami.ubuntu.id}"
+    instance_type = "t2.micro"
+
+    tags {
+        Name = "identification"
+    }
+  
 }
